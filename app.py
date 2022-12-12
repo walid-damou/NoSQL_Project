2b5a94a39341880a89b5a6bb82d341e3d6c78fa7
walid damou <walid.damou.2015@gmail.com>
2022-12-12 23:05:54 +0100

    routes

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
+#----------------------------Login--------------------------------#
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, walid!</p>"
+def login():
+    return render_template("login.html")
+
+#----------------------------/Login--------------------------------#
+
+#----------------------------HOME--------------------------------#
+
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
+#----------------------------/HOME--------------------------------#
+
+#----------------------------ADD Prod--------------------------------#
+
+@app.route("/addProduit")
+def AddProd():
+    return render_template("addProduct.html")
+
+#----------------------------/ADD Prod--------------------------------#
+
+#----------------------------Update Prod--------------------------------#
+
+@app.route("/addProduit")
+def UpdateProd():
+    return render_template("updateProduct.html")
+
+#----------------------------/Update Prod--------------------------------#
